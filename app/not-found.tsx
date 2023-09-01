539c16ef4fcb8c89def098cdb12be9317892766f
Angelo <angelo100492@gmail.com>
2023-09-01 14:45:47 -0300

    Review message in 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,9 +4,9 @@ export default function NotFound() {
     return (
         <div className="flex items-center justify-center h-screen" >
         <div className="text-center">
-            <h1 className="text-black mb-4 ">Página no encontrada</h1>
+            <h1 className="text-black mb-4 ">Esta página aparece cuando estás perdido :-)</h1>
             <Link href="/" className="bg-red-500 text-white p-2 rounded">
-                Vuelve al inicio
+                Volver al inicio
             </Link>
         </div>
         </div>
